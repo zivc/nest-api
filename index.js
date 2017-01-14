8b986baaa0f4c302ed176602a9356da63a3852d1
Taylor, Ash <ashley.taylor@sungard.com>
2017-01-14 13:17:08 +0000

    codes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const config = require('./config'),
+var config = require('./config'),
   nestApi = require('./lib/nest'),
   nest = new nestApi(config);
 
 nest.login()
-	.then(console.log.bind(console))
-	.catch(console.log.bind(console));
+	.then(nest.cameras())
+	.then(function(obj) {
+		console.log(obj);
+		//console.log(JSON.parse(obj,null,4));
+	})
+	.catch(function(err) {
+		console.log('there has been an error', err);
+	});
 
 // nest.login(config.token)
 //   .then(nest.cameras)
